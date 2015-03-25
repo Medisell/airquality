84bcddb114923cb99f08963e59b2668c91816803
Loizos Papaloizou <lpapaloizou@medisell.com>
2015-03-25 14:17:30 +0200

    Revert "Airquality application / Console version binaries"
    This reverts commit cb221ee01d33b8c1f732984a7f8515111b51e080.

--- airodis-console/test2.sql
+++
@@ -1,2 +0,0 @@
-SELECT [Date_Time],[C1] FROM [AIRODIS_NEW].[dbo].[D0903083147705#NicTra] 
-				WHERE Report=0  AND Date_Time >= '2011-01-01 00:00' AND Date_Time <= '2015-01-31 00:00' AND C1DStatus='144'
